22da2a68074b73e649e79347c29a55ef054a83e2
Alexandre Gotte <alexandre.gotte@riminder.net>
2018-04-17 17:53:54 +0200

    Added autologin ang user config in ~/.riminder/userData.json

--- main.js
+++ main.js
@@ -4,24 +4,109 @@ const BrowserWindow = electron.BrowserWindow
 
 const url = require('url')
 const path = require('path')
-// const log = require('electron-log')
+const fs = require('fs')
+const fx = require('mkdir-recursive')
+const log = require('electron-log')
+const os = require('os')
 
 let mainWindow
+
+class UserData {
+  constructor () {
+    this.team = undefined
+  }
+}
+
+const linuxUserDataPath = path.join('/home', os.userInfo().username, '.riminder/userData.json').toString()
+
+function getUserDataFromfile (filePath) {
+  let userData = new UserData()
+
+  try {
+    let fileData = fs.readFileSync(filePath, {encoding: 'utf8', flag: 'r'})
+    log.info('ud', 'parse')
+    let tmpuserData = JSON.parse(fileData.toString())
+    Object.assign(userData, tmpuserData)
+  } catch (e) {
+    log.error('get ud', 'err: ' + e)
+    return new UserData()
+  }
+  return userData
+}
+
+function saveUserDataToFile (ud, filePath) {
+  let usString = JSON.stringify(ud)
+
+  let fp = path.parse(filePath)
+  log.info('save', 'fp.dir: ' + fp.dir)
+  fx.mkdir(fp.dir, (err) => {
+    if (err) {
+      log.error('save', 'die mkdir: ' + err)
+      return
+    }
+
+    fs.writeFile(filePath, usString, {
+      mode: fs.constants.S_IRWXU | fs.constants.S_IRWXG
+    }, (err) => {
+      if (err) {
+        log.error('userData', 'Cannot write ud: ' + err)
+      }
+    })
+  })
+}
+
+console.log('hey')
+let userData = getUserDataFromfile(linuxUserDataPath)
+
+const signinUrl = url.format({
+  protocol: 'https:',
+  hostname: 'www.riminder.net',
+  pathname: 'signin/team/'
+})
+
+function getDirectLoginUrl (team) {
+  let _hostname = team + '.riminder.net'
+  let res = url.format({
+    protocol: 'https:',
+    hostname: _hostname,
+    pathname: 'dashboard'
+  })
+  return res
+}
 
 function createWindow () {
   mainWindow = new BrowserWindow({width: 1500,
     height: 1000,
+    show: false,
     icon: path.join(__dirname, 'assets/icons/png/64x64.png'),
     titleBarStyle: 'hidden'})
 
-  mainWindow.loadURL(url.format({
-    protocol: 'https:',
-    hostname: 'www.riminder.net',
-    pathname: 'signin/team/'
-  }))
+  let signurl = signinUrl
+  if (userData.team !== undefined) {
+    signurl = getDirectLoginUrl(userData.team)
+  }
+
+  mainWindow.loadURL(signurl)
 
   mainWindow.on('closed', () => {
+    console.log('pomme')
     mainWindow = null
+  })
+
+  mainWindow.on('ready-to-show', () => {
+    mainWindow.show()
+  })
+
+  mainWindow.webContents.on('did-finish-load', () => {
+    console.log('url: ' + mainWindow.webContents.getURL())
+    let currentUrl = url.parse(mainWindow.webContents.getURL())
+    console.log('host: ' + currentUrl.hostname)
+    let regex = new RegExp(/^(.+)\.riminder\.net$/)
+    if (regex.test(currentUrl.hostname) && RegExp.$1 !== 'www') {
+      userData.team = RegExp.$1
+      log.info('url', 'heyy team is: ' + userData.team)
+      saveUserDataToFile(userData, linuxUserDataPath)
+    }
   })
 }
 
