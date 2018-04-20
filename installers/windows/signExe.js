653be9189dcd3c31c35e77c88142c0d6e170a658
Alexandre Gotte <alexandre.gotte@riminder.net>
2018-04-20 14:05:40 +0200

    Updated signExe

--- installers/windows/signExe.js
+++ installers/windows/signExe.js
@@ -1,12 +1,11 @@
 const path = require('path')
 const commandLineArgs = require('command-line-args')
-const cmd = require('node-cmd')
+const childProcess = require('child_process')
+const log = require('log')
 
 let args = commandLineArgs([
   {name: 'cert-path', type: String, defaultOption: true},
-  {name: 'cert-passw', type: String, defaultValue: '""'},
-  {name: 'exe', type: String}
-], { camelCase: true})
+  {name: 'cert-passw', type: String, defaultValue: '""'}], { camelCase: true })
 
 let signCertPath
 let signCertPassword
@@ -14,6 +13,24 @@ let signCertPassword
 signCertPath = args.certPath
 signCertPassword = args.certPassw
 
-cmd.run(`
-  npm run package-win`)
-//  signtool sign /t http://timestamp.verisign.com/scripts/timstamp.dll /f "${path.resolve(signCertPath)}" /p ${signCertPassword} release-builds/riminder-win32-ia32/riminder.exe`)
+let createPack = childProcess.spawnSync('npm', ['run', 'package-win'], {stdio: 'inherit'})
+// createPack.on('error', function (err) {
+//   if (err) {
+//     throw err
+//   }
+// })
+// createPack.on('close', function () {
+//   log.info('Package created!')
+// })
+
+let signPack = childProcess.spawnSync('signtool', ['sign',
+  '/t', 'http://timestamp.verisign.com/scripts/timstamp.dll',
+  '/f', `"${path.resolve(signCertPath)}"`,
+  '/p', `${signCertPassword}`,
+  'release-builds/riminder-win32-ia32/riminder.exe'], {stdio: 'inherit'})
+// signPack.on('error', function (err) {
+//   throw err
+// })
+// signPack.on('close', function () {
+//   log.info('Package created!')
+// })
