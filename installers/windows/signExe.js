fad4143dc47cc5fcccde701c8ac85fab4594e79d
Alexandre Gotte <alexandre.gotte@riminder.net>
2018-04-20 03:53:43 -0700

    Fixed signExe

--- installers/windows/signExe.js
+++ installers/windows/signExe.js
@@ -3,10 +3,10 @@ const commandLineArgs = require('command-line-args')
 const cmd = require('node-cmd')
 
 let args = commandLineArgs([
-  {name: 'certPath', type: String},
-  {name: 'certPassw', type: String},
+  {name: 'cert-path', type: String, defaultOption: true},
+  {name: 'cert-passw', type: String, defaultValue: '""'},
   {name: 'exe', type: String}
-])
+], { camelCase: true})
 
 let signCertPath
 let signCertPassword
@@ -15,5 +15,5 @@ signCertPath = args.certPath
 signCertPassword = args.certPassw
 
 cmd.run(`
-  npm run package-win
-  signtool sign /t http://timestamp.verisign.com/scripts/timstamp.dll /f "${path.resolve(signCertPath)}" /p ${signCertPassword} release-builds/riminder-win32-ia32/riminder.exe`)
+  npm run package-win`)
+//  signtool sign /t http://timestamp.verisign.com/scripts/timstamp.dll /f "${path.resolve(signCertPath)}" /p ${signCertPassword} release-builds/riminder-win32-ia32/riminder.exe`)
