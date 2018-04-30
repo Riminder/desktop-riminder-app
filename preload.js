08bf68f61cacb857b8fbe2a544cd3ed36d4f141b
Alexandre Gotte <alexandre.gotte@riminder.net>
2018-04-30 15:45:21 +0200

    Added fix profile

--- preload.js
+++ preload.js
@@ -18,15 +18,29 @@ function getProfileOverlayIFrame (profileOverlay) {
   return elements[0]
 }
 
+function generateProfileOverlayIFrame (parent) {
+  let iframe = document.createElement('iframe')
+
+  iframe.setAttribute('frameborder', '0')
+  // iframe.setAttribute('src', '')
+}
+
+/* <iframe frameborder="0" src="https://s3-eu-west-1.amazonaws.com/riminder-documents-eu/resume/4ddf07a67c01938c9a48bcbcc9c73db0599673cd.pdf" target="_parent" allowfullscreen="" height="140vh" name="" width="100%" style="position: relative; display: initial; height: 140vh; width: 100%;"></iframe> */
+
 function windowLoadedHandler () {
   document.body.addEventListener('click', () => {
     let profileOverlay = getProfileOverlay()
     console.log('clicked yo ' + profileOverlay)
     if (profileOverlay !== null) {
-      let ovIframe = getProfileOverlayIFrame(profileOverlay)
-      if (ovIframe !== null) {
-        ovIframe.remove()
-      }
+      setTimeout(() => {
+        let ovIframe = getProfileOverlayIFrame(profileOverlay)
+        if (ovIframe !== null) {
+          console.log('pimped!')
+          let ovParent = ovIframe.parentElement
+          ovIframe.remove()
+
+        }
+      }, 50)
     }
   })
 }
